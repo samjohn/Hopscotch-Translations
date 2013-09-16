b39b6cc3ea05b6a388240cb49aab811ad7af1ee8
Samantha John <samantha.m.john@gmail.com>
2013-09-16 17:25:18 -0400

    Better response for gengo callback

--- app/controllers/gengo_responses_controller.rb
+++ app/controllers/gengo_responses_controller.rb
@@ -1,10 +1,13 @@
 class GengoResponsesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
   def index
     @gengo_responses = GengoResponse.all
   end
 
   def create
-    g = GengoResponse.new(resp_text: params)
+    puts params
+    g = GengoResponse.new(resp_text: params.to_json)
     g.save
+    render text: "success"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Translations::Application.routes.draw do
   root to:"gengo_responses#index"
   resources :gengo_responses
+
 end
