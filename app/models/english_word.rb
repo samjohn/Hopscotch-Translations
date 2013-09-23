8f8010b0aef6717d60bc87a31ab06b403469b832
Samantha John <samantha.m.john@gmail.com>
2013-09-23 17:10:28 -0400

    Add relationship between english and foreign words.

--- app/models/english_word.rb
+++ app/models/english_word.rb
@@ -1,3 +1,8 @@
 class EnglishWord < ActiveRecord::Base
   validates_uniqueness_of :translatable_string
+  has_many :foreign_words, primary_key: :translatable_string, foreign_key: :translatable_string
+
+  def translated_string
+    translatable_string
+  end
 end
--- app/models/foreign_word.rb
+++ app/models/foreign_word.rb
@@ -1,3 +1,7 @@
 class ForeignWord < ActiveRecord::Base
+  belongs_to :english_word, primary_key: :translatable_string, foreign_key: :translatable_string
   validates_uniqueness_of :translatable_string, :scope => :language
+  validates :english_word, presence: true
+
+  delegate :comment, to: :english_word
 end
