2b67358ec29c8699ee446843c294f46f4a5e7a4c
Samantha John <samantha.m.john@gmail.com>
2013-09-27 16:37:33 -0400

    Remove log on gengo response.

--- app/controllers/gengo_responses_controller.rb
+++ app/controllers/gengo_responses_controller.rb
@@ -1,7 +1,6 @@
 class GengoResponsesController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
   def create
-    Rails.logger.info params
     render text: "success"
   end
 end
