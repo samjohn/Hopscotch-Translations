8562030d8338192136c5e430980d3287c2806497
Samantha John <samantha.m.john@gmail.com>
2013-09-30 18:21:04 -0400

    make hebrew the first langauge

--- config/initializers/files.rb
+++ config/initializers/files.rb
@@ -1,5 +1,5 @@
 ENCODING = "UTF-16"
 
-LANGUAGES = %w(cs da de el es fr ja ko nl pt ru sv zh zh-tw fi he)
+LANGUAGES = %w(he cs da de el es fr ja ko nl pt ru sv zh zh-tw fi)
 LANGUAGE_FOLDER_MAPPINGS = {"zh" => "zh-Hans", "zh-tw" => "zh-Hant"}
 
