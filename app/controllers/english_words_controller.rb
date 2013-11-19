5aabf7456762c14419f3ff92646fdba487c1e519
Samantha John <samantha.m.john@gmail.com>
2013-11-19 15:34:29 -0500

    Add a form for new english word.

--- app/controllers/english_words_controller.rb
+++ app/controllers/english_words_controller.rb
@@ -6,4 +6,18 @@ class EnglishWordsController < ApplicationController
   def localizable
     index
   end
+
+  def create
+    attrs = ActionController::Parameters.new(params[:english_word])
+
+    attrs.permit!
+
+    e = EnglishWord.new(attrs)
+    if (e.save!)
+      redirect_to english_words_path
+    else
+      flash :error, e.errors.full_messages
+      render :index
+    end
+  end
 end
