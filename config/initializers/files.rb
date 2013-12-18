eaaddd31c30d761be48acfa219465753932ad686
Samantha John <samantha.m.john@gmail.com>
2013-12-18 11:09:46 -0500

    Remove all languages but spanish and chinese

--- config/initializers/files.rb
+++ config/initializers/files.rb
@@ -1,5 +1,5 @@
 ENCODING = "UTF-16"
 
-LANGUAGES = %w(he cs da de el es fr ja ko nl pt ru sv zh zh-tw fi)
+LANGUAGES = %w(es zh zh-tw)
 LANGUAGE_FOLDER_MAPPINGS = {"zh" => "zh-Hans", "zh-tw" => "zh-Hant"}
 
