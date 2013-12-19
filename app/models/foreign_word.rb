16a22bae7f0babca98c4468f32c3fc6366ba5970
Samantha John <samantha.m.john@gmail.com>
2013-12-19 10:09:52 -0500

    Revert "Change the meaning of needs_translation_job to include things that have nnot yet been translated"
    This reverts commit 5986705942b9140a56d76e3d3555df735371c786.

--- app/models/foreign_word.rb
+++ app/models/foreign_word.rb
@@ -105,7 +105,7 @@ class ForeignWord < ActiveRecord::Base
 
   private
   def doesnt_need_translation_job?
-    translatable_string.length < 2 || !valid?
+    gengo_job || translatable_string.length < 2 || !valid?
   end
 
 end
