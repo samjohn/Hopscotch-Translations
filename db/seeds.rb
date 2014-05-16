5c742093a4df495730e3d65e8a9e4445d7864805
Samantha John <samantha.m.john@gmail.com>
2014-05-16 14:29:27 -0400

    Don't delete all the foreign words

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-ForeignWord.delete_all_app_strings
 EnglishWord.delete_all_app_strings
 file_root_url = "https://s3.amazonaws.com/hopscotchtranslations/#{Rails.env}/en.lproj"
 
@@ -15,17 +14,17 @@ english_words.each do |line|
   EnglishWord.create_word_from_line(line)
 end
 
-#create words for other languages
-languages = LANGUAGES
+# create words for other languages
+# languages = LANGUAGES
 
-languages.each do |language|
-  language_words = ForeignWord.get_words_for_language(language, file_root_url, encoding)
+# languages.each do |language|
+  # language_words = ForeignWord.get_words_for_language(language, file_root_url, encoding)
 
-  language_words.each do |line|
-    next if line.length == 0
+  # language_words.each do |line|
+    # next if line.length == 0
 
-    ForeignWord.create_foreign_word_from_data(line, language)
-  end
-end
+    # ForeignWord.create_foreign_word_from_data(line, language)
+  # end
+# end
 
 EnglishWord.create_foreign_words
